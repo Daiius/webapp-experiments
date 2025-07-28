d657f22ba3bb62898bdf39172382cea7deeaa4c0
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-07-28 23:59:56 +0900

    body全体にflex flex-colを設定
    flexのみでは想定通りのレイアウトにならないので修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={'antialiased'}
-      >
+      <body className={'antialiased flex flex-col min-h-screen'} >
         <Header />
         {children}
         <Footer />
