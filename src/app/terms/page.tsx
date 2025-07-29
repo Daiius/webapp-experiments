54ab57cacb94a5a98557a9e49d057ed0c56d405a
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-07-29 09:26:38 +0900

    スクロール関連のよくある処理から問題のありそうなコードを抽出
    バグ対処用のコードも追加したが、一旦コメントアウト状態にしている
    この時点ではスクロール挙動を再現できる

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,14 +1,42 @@
-
+'use client'
+import { useEffect, useState, } from 'react' 
 import clsx from 'clsx'
 import { indexes } from '@/data/terms'
 
 import { Link } from '@/components/common/Link'
+import { StickableSearchBar } from '@/components/terms/StickableSearchBar'
 
-export default async function TermsPage() {
+/**
+ * ModalやDrawerなど、後ろのbody等の要素をスクロールさせない様にするよくある処理が
+ * 「戻る」ボタンでこのページに遷移した際に毎回トップにスクロールする問題と
+ * 何か関係がないか確認するための関数
+ *
+ * よく使われる処理の一部を取り出して調査
+ */
+const experimentalScrollFunction = () => {
+  const scrollY = parseInt(document.body.style.top || '0')
+  window.scrollTo(0, scrollY * -1)
+}
+
+export default function TermsPage() {
+  const [dummy, _] = useState<boolean>(false)
+  //const [mounted, setMounted] = useState<boolean>(false)
+
+  //useEffect(() => setMounted(true), [])
+
+  useEffect(() => {
+    //if (mounted) {
+      setTimeout(() => {
+        experimentalScrollFunction()
+      }, 100)
+    //}
+  }, [dummy])
+
   return (
     <div className={clsx(
       'flex-auto flex flex-col items-center',
     )}>
+      <StickableSearchBar />
       <div className='p-2 flex flex-wrap gap-2'>
         {indexes.map(index => 
           <div 
@@ -28,7 +56,7 @@ export default async function TermsPage() {
       <div className='w-full'>
         {indexes.map(index =>
           <section key={index} id={index} className='scroll-mt-16'>
-            <div className='bg-slate-200 p-1 top-16 sticky'>{index}行</div>
+            <div className='bg-slate-200 p-1 top-32 sticky'>{index}行</div>
             <div className='flex flex-col sm:flex-row sm:flex-wrap sm:justify-between'>
               {[...new Array(10)].map((_, i) => 
                 <div 
